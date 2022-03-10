8ece7f91025430803ad9253739d87eb1ec8cce51
Resaw <resavv@mail.ru>
2022-03-11 02:14:47 +0300

    litle bit refactoring for chessboard basic js 5

--- education/scripts/basic5.js
+++ education/scripts/basic5.js
@@ -1,36 +1,32 @@
-const modal = document.querySelector('.modal-body')
+const chessboard = document.getElementById('chessboard')
 const table = document.createElement('table')
 const tbody = document.createElement('tbody')
-
-document.querySelector('.modal-body').appendChild(table)
-document.querySelector('table').appendChild(tbody)
 const letters = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'J', 'H']
-let numbers = 9
+
+chessboard.appendChild(table)
+table.appendChild(tbody)
 
 for (let row=0; row<=8; row++) {
-    let tr = document.createElement('tr')
+    const tr = document.createElement('tr')
     document.querySelector('tbody').appendChild(tr)
     for (let col=0; col<=8; col++) {
-        let th = document.createElement('th')
-        let td = document.createElement('td')
-        /* isBlack++ */
+        const th = document.createElement('th')
+        const td = document.createElement('td')
         if (row == 0) {
             document.querySelector('tr').appendChild(th)
             th.innerText = letters[col]
-            th.style.padding = ".5em"
+            th.style.width = "30px"
             th.style.textAlign = "center"
         } else {
             document.querySelectorAll('tr').forEach(el => {
                 if (col == 0) {
                     el.appendChild(th)
-                    th.innerText = numbers - row
-                    th.style.padding = ".5em"
+                    th.innerText = 9 - row
+                    th.style.height = "30px"
+                    th.style.textAlign = "center"
                 } else {
                     el.appendChild(td)
                     td.style.border = "1px solid"
-                    td.style.width = "5em"
-                    td.style.height = "5em"
-                    td.style.textAlign = "center"
                     if ((col - row) % 2 != 0) {
                         td.style.backgroundColor = "black"
                     }  
@@ -39,4 +35,3 @@ for (let row=0; row<=8; row++) {
         }
     }
 }
-
